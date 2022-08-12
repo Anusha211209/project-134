b55a84432701d634c14ff65d5d53fb7bbf32240c
Anusha211209 <84378742+Anusha211209@users.noreply.github.com>
2022-08-12 17:12:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+img = "";
+song= "";
+
+function preload(){
+    img = loadImage('baby.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            
+            document.getElementById("stauts").innerHTML = "STATUS: OBJECTS DETECTED";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResults(error, results){
+    if (error){
+        console.error(error);
+    }else{
+        console.log(results);
+        objects = results;
+    }
+
+}
